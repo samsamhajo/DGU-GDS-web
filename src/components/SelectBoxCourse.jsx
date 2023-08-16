2cedd5a1450456f2c33d04596c7e7e976e5a40de
joseph <joseph@192.168.2.17>
2023-08-17 06:29:27 +0700

    design&feat: 과정 셀렉트 박스 구현, 디자인은 추후 수정

---
+++ src/components/SelectBoxCourse.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+
+const SelectBoxCourse = ({ setCourse }) => {
+  const [value, setValue] = useState(""); // 선택된 값
+  const [isClicked, setIsClicked] = useState(false); // 클릭된 상황 유무
+
+  const categoryList = ["심화과정", "일반과정"]; // 과정 리스트
+
+  /** value 바뀔 때마다 과정 세팅 */
+  useEffect(() => {
+    setCourse(value);
+  }, [value]);
+
+  return (
+    <div
+      onClick={() => setIsClicked(!isClicked)}
+      onBlur={() => setIsClicked(false)}
+      tabIndex={0}
+      className={
+        isClicked
+          ? "w-[120px] h-[24px] font-medium text-[12px] overflow-visible cursor-default"
+          : "w-[120px] h-[24px] font-medium text-[12px] overflow-hidden cursor-default"
+      }
+    >
+      <div className="flex items-center justify-between px-[3px] border-[1px] rounded-[3px] w-[120px] h-[24px]">
+        <div>{value}</div>
+        {/** 여기 이미지 들어가야함 */}
+      </div>
+      {categoryList.map((i, j) => {
+        return (
+          <div
+            onClick={() => setValue(i)}
+            key={`fdvc${j}`}
+            className="flex items-center px-[3px] border-x-[1px] border-b-[1px] rounded-[3px] w-[120px] h-[24px]"
+          >
+            {i}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default SelectBoxCourse;

24042bf23a8e2066c4714437cb5d80e440614c4a
joseph <joseph@192.168.2.17>
2023-08-17 06:31:01 +0700

    chore: 셀렉트 박스 컴포넌트 export 설정

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,5 @@
 export { default as GlobalNavBar } from "./GlobalNavBar";
+export { default as SelectBoxDepartment } from "./SelectBoxDepartment";
+export { default as SelectBoxCategory } from "./SelectBoxCategory";
+export { default as SelectBoxCourse } from "./SelectBoxCourse";
+export { default as SelectBoxCondition } from "./SelectBoxCondition";

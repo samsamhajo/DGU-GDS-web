98530d260dc90e7f4c95a5479f031dc0b4607f46
joseph-wee <starlightkor@gmail.com>
2023-10-16 20:49:04 +0900

    add: asset들 추가

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,5 @@
 export { default as expand_less } from "./expand_less.svg";
 export { default as expand_more } from "./expand_more.svg";
-export { default as test } from "./test.png";
+export { default as arrow_top } from "./arrow_top.svg";
+export { default as arrow_bottom } from "./arrow_bottom.svg";
+export { default as cancel } from "./cancel.svg";
